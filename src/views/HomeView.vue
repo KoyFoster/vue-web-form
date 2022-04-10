f655d27a13ab30e51af3a764f6c63af55d511b3a
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-04-10 18:08:09 -0500

    Refs for reactivity intro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,24 +2,28 @@
   <h1>This is the Home page</h1>
   <p ref="para">Setup Name: {{ name }} Setup Age: {{ age }}</p>
   <button @click="handleClick">Click me</button>
+  <button @click="age++">Grow up</button>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
 export default {
+  name: "HomeView",
   // setup runs before everything else
   setup() {
     console.log("setup");
 
     const para = ref(null);
 
-    let name = "mario";
-    let age = 30;
+    const name = ref("mario");
+    const age = ref(30);
 
     const handleClick = () => {
       console.log("you clicked me:", para, para.value);
       para.value.classList.add("test");
-      para.value.textContent = '[ERROR]'
+      para.value.textContent = "[ERROR]";
+      name.value = "ppop";
     };
 
     // return data to template
