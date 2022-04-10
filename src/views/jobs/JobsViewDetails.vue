b1479a39e68a12c90915df03a9b1433e2e35914e
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-04-10 17:28:52 -0500

    fetch handling via vue intro

--- src/views/jobs/JobsViewDetails.vue
+++ src/views/jobs/JobsViewDetails.vue
@@ -1,16 +1,27 @@
 <template>
-  <h1>This is Job Details</h1>
-  <p>The job id is {{ id }}.</p>
+  <div v-if="job !== null">
+    <h1>Title: {{ job.title }}</h1>
+    <p>Details: {{ job.details }}</p>
+  </div>
+  <div v-else><h1>Loading...</h1>.</div>
 </template>
 
 <script>
 export default {
   props: ["id"],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log({ data });
+        this.job = data;
+      });
+  },
 };
 </script>
 
