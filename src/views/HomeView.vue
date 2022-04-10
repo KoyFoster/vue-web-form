47fb9b451ba02fa67449a1833d8be3322dc24444
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-04-10 17:43:26 -0500

    Composition API intro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,33 @@
 <template>
-    This is the Home page
+  <h1>This is the Home page</h1>
+  <h2>Setup Name: {{ name }} Setup Age: {{ age }}</h2>
+  <button @click='handleClick'>Click me</button>
 </template>
+
+<script>
+export default {
+  // setup runs before everything else
+  setup() {
+    console.log("setup");
+    let name = "mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log("you clicked me");
+    };
+
+    // return data to template
+    // non-reactive data
+    return {
+      name: name,
+      age: age,
+      handleClick
+    };
+  },
+
+  // reactive data
+  data() {
+    return {};
+  },
+};
+</script>
