83f5b8c5c53e8b7ec6912fab9736e011f8f61baf
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-04-09 20:02:09 -0500

    Programmatic Navigation intro

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,30 @@
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link> |
     <router-link to="/Signup">Sign Up</router-link>
   </div>
+
+  <!-- Navigations -->
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go Back</button>
+  <button @click="forward">Got Forward</button>
+
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: "Home" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -33,5 +55,13 @@
   color: #42b983;
   background: white;
   border-radius: 2px;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
